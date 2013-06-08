2b955fbc6ed87e25d12995b3d7d248daac1d52a1
tsmgeorge <tsmgeo@hotmail.com>
2013-06-08 20:07:33 +0300

    ds

---
+++ src/jenkins/helloJenkins/helojenkins.java
@@ -0,0 +1,15 @@
+package jenkins.helloJenkins;
+
+public class helojenkins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello jenkins!!!");	
+     
+     System.out.println("Hello jenkins!!!");
+	}
+
+}
